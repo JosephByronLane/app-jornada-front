c301e1426c607fcdc116db5ecde713838e7cc583
= <=>
2024-11-26 18:25:25 -0600

    changing initial script

--- src/preprocessing/script.py
+++ src/preprocessing/script.py
@@ -1,8 +1,8 @@
 import csv
 import json
 
-csv_file = 'input.csv' #nombre del archivo a parsear
-json_file = 'output.json' #nombre del archivo parseado
+csv_file = 'pagos.csv' #nombre del archivo a parsear
+json_file = 'output2.json' #nombre del archivo parseado
 
 output_data = {}
 
@@ -18,8 +18,8 @@ with open(csv_file, 'r', encoding='utf-8') as file:
         email = row[2]
         key = email.split('@')[0]  #sacamos la matricula del correo, separandolo en 2: uno antes de la @ y uno despues.
         output_data[key] = {
-            "Nombre": row[1].strip(),
-            "Correo": row[2].strip(),
+            "Nombre": row[1].strip() + row[2].strip() + row[3].strip(),
+            "Correo": row[4].strip(),
             "Registro de pago por": row[3].strip(),
             "Fecha": row[4].strip()
         }
